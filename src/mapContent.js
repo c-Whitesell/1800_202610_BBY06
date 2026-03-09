73b79b256d569243c4fb38dd36716d0f58747c6b
c-whitesell <connor.whitesell02@gmail.com>
2026-03-09 09:07:45 -0700

    Adjust navbar

--- src/mapContent.js
+++ src/mapContent.js
@@ -1,13 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import "./styles/style.css";
-import "leaflet/dist/leaflet.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import './styles/style.css';
+import 'leaflet/dist/leaflet.css';
 
-import * as L from "leaflet";
+import * as L from 'leaflet';
 
-const map = L.map("map").setView([49.236, -123.025], 13);
+const map = L.map('map').setView([49.236, -123.025], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -38,7 +38,7 @@ async function getNearbyRestaurants(lat, lon, radius = 1000) {
     const data = await response.json();
     return data.elements; // Array of restaurants [2]
   } catch (error) {
-    console.error("Error fetching data from Overpass API:", error);
+    console.error('Error fetching data from Overpass API:', error);
   }
 }
 
@@ -63,47 +63,47 @@ getNearbyRestaurants(49.236, -123.025, 6000).then((restaurants) => {
 map.locate({ setView: true, maxZoom: 16 });
 
 function onLocationFound(e) {
-    var radius = e.accuracy;
+  var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map);
+  L.marker(e.latlng).addTo(map);
 
-    L.circle(e.latlng, radius).addTo(map);
+  L.circle(e.latlng, radius).addTo(map);
 }
 
 map.on('locationfound', onLocationFound);
 
 L.Control.MyCustomButton = L.Control.extend({
-    options: {
-        position: 'bottomleft', // Position the control in the bottom left
-    },
+  options: {
+    position: 'bottomleft', // Position the control in the bottom left
+  },
 
-    onAdd: function (map) {
-        // Create the button element
-        let container = L.DomUtil.create('button', 'my-custom-button');
-        container.innerHTML = 'My Button';
-        container.style.height = '50px';
-        container.style.width = '100px';
-        container.style.borderRadius = '50px';
-        container.style.backgroundColor = 'blue';
-        container.style.color = 'white';
-        // Add a click event listener
-        L.DomEvent.on(container, 'click', function (e) {
-            alert('Button clicked!');
-            // add on click pop up here
-            // Prevent event from propagating to the map
-            L.DomEvent.stop(e);
-        });
+  onAdd: function (map) {
+    // Create the button element
+    let container = L.DomUtil.create('button', 'modalButton');
+    container.innerHTML = 'My Button';
+    container.style.height = '50px';
+    container.style.width = '100px';
+    container.style.borderRadius = '50px';
+    container.style.backgroundColor = 'blue';
+    container.style.color = 'white';
+    // Add a click event listener
+    L.DomEvent.on(container, 'click', function (e) {
+      alert('Button clicked!');
+      // add on click pop up here
+      // Prevent event from propagating to the map
+      L.DomEvent.stop(e);
+    });
 
-        // Prevent click events on the button from moving the map
-        L.DomEvent.disableClickPropagation(container);
+    // Prevent click events on the button from moving the map
+    L.DomEvent.disableClickPropagation(container);
 
-        return container;
-    },
+    return container;
+  },
 
-    onRemove: function (map) {
-        // Clean up event listeners if the control is removed
-        L.DomEvent.off(this._container, 'click', function () {});
-    },
+  onRemove: function (map) {
+    // Clean up event listeners if the control is removed
+    L.DomEvent.off(this._container, 'click', function () {});
+  },
 });
 
 // Add the new control to the map
